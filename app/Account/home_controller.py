6463fe3d7adfa104aa3f16fec7d0b46a3d08eec6
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2024-11-24 10:36:56 +0700

    add function update, edit, delete, create, filter, search Account

--- app/Account/home_controller.py
+++ app/Account/home_controller.py
@@ -2,4 +2,25 @@ from flask import render_template, current_app, url_for
 from flask import jsonify
 
 def home():
-    return render_template('account.html')
+
+    accounts = [
+        {"name": "Vietcombank", "type": "bank", "balance": 1},
+        {"name": "SeABank", "type": "bank", "balance": 2},
+        {"name": "TPBank", "type": "bank", "balance": 3},
+        {"name": "BIDV", "type": "bank", "balance": 5},
+        {"name": "ZaloPay", "type": "ewallet", "balance": 1},
+        {"name": "VNPay", "type": "ewallet", "balance": 2},
+        {"name": "Tiền mặt", "type": "cash", "balance": 10}
+    ]
+    bank_accounts = [account for account in accounts if account["type"] == "bank"]
+    ewallet_accounts = [account for account in accounts if account["type"] == "ewallet"]
+    cash_accounts = [account for account in accounts if account["type"] == "cash"]
+
+
+    total_balance = sum(account["balance"] for account in bank_accounts) + sum(account["balance"] for account in ewallet_accounts) + sum(account["balance"] for account in cash_accounts)
+    return render_template('account.html',
+                         bank_accounts=bank_accounts,
+                         ewallet_accounts=ewallet_accounts,
+                         cash_accounts=cash_accounts,
+                         total_balance=total_balance,
+                         accounts=accounts)
--- static/js/account.js
+++ static/js/account.js
@@ -39,3 +39,47 @@ if (addTransferBtn && addTransferModal && closeTransferBtn && cancelTransferBtn)
 } else {
     console.error('Xem lỗi ở file account.js line 25-31');
 }
+
+const filterSelect = document.querySelector('.filter-select');
+const searchInput = document.querySelector('.search-input');
+const deleteBtn = document.querySelector('.delete-btn');
+const editBtn = document.querySelector('.edit-btn');
+const editAccountModal = document.querySelector('.edit-account-modal');
+const closeEditAccountBtn = document.querySelector('.close-edit-account-btn');
+const cancelEditBtn = document.querySelector('.cancel-edit-btn');
+
+if (filterSelect && searchInput) {
+    filterSelect.addEventListener('change', () => {
+        window.location.href = `/account?filter=${filterSelect.value}`;
+    });
+
+    searchInput.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            window.location.href = `/account?search=${searchInput.value}`;
+            }
+        });
+} else {
+    console.error('Xem lỗi ở file account.js line 47-51');
+}
+
+
+if (deleteBtn && editBtn && closeEditAccountBtn && cancelEditBtn) {
+    deleteBtn.addEventListener('click', () => {
+        console.log('delete');
+    });
+
+    editBtn.addEventListener('click', () => {
+        editAccountModal.classList.add('active');
+    });
+
+    closeEditAccountBtn.addEventListener('click', () => {
+        editAccountModal.classList.remove('active');
+    });
+
+    cancelEditBtn.addEventListener('click', () => {
+        editAccountModal.classList.remove('active');
+    });
+} else {
+    console.error('Xem lỗi ở file account.js line 66-71');
+}
+
