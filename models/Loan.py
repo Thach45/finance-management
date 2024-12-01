fdf99746829cd9ec516b66209c984ec13c8ea9d4
anhhiii <thutrangyuri510@gmail.com>
2024-12-01 17:12:02 +0700

    add loan

---
+++ models/Loan.py
@@ -0,0 +1,39 @@
+from Transaction import Transaction
+from Account import Account
+from Category import Category
+from datetime import date
+
+class Loan(Transaction):
+    def __init__(self, account, transactionType, money=0, transactionDate= date.today(), note="", spender ="", dateDue= date.today(), rate = 0):
+        super().__init__(account, transactionType, money, transactionDate, note)
+        self.__spender = spender
+        self.__dateDue = dateDue
+        self.__rate = rate
+
+    @property
+    def spender(self):
+        return self.__spender
+    @spender.setter
+    def spender(self, value):
+        self.__spender = value
+
+    @property
+    def dateDue(self):
+        return self.__dateDue
+    @dateDue.setter
+    def dateDue(self, value:date):
+        self.__dateDue = value
+
+    @property
+    def rate(self):
+        return self.__rate
+    @rate.setter
+    def rate(self, value:float):
+        if value >= 0:
+            self.__rate = value
+        else:
+            raise ValueError("Rate must be a positive value")
+        
+    
+loan1 = Loan(Account(), Category(), 100, date.today(), "loan", "ri", "15/1/2025", 0.001)
+print(loan1.money)
