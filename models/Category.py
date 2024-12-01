2faa53b1c1e45012ba5bc4d6153afe164e25555b
Huy Hoàng Ngô <ngohuyhoang1808@gmail.com>
2024-12-01 10:38:04 +0700

    create class jar, category

---
+++ models/Category.py
@@ -0,0 +1,61 @@
+from Jar import Jar
+
+jar1 = Jar("Chi tiêu cần thiết")
+jar2 = Jar("Tiết kiệm dài hạn")
+jar3 = Jar("Quỹ giáo dục")
+jar4 = Jar("Quỹ hưởng thụ")
+jar5 = Jar("Quỹ tự do tài chính")
+jar6 = Jar("Quỹ từ thiện")
+
+class Category:
+    def __init__(self,name = '',jar=Jar()):
+        self.__name = name
+        self.__money = 0
+        self.__moneyGoal = 1000000
+        self.__jar = jar
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self,value):
+        self.__name = value
+
+    @property
+    def money(self):
+        return self.__money
+    @money.setter
+    def money(self,value):
+        if value > 0:
+            self.__money = value
+        else: 
+            return False
+    
+    @property
+    def moneyGoal(self):
+        return self.__moneyGoal
+    
+    def SetMoneyGoal(self,value):
+        if value>0:
+            self.__moneyGoal = value
+        else: 
+            return False
+        
+    @property
+    def jar(self):
+        return self.__jar
+    
+    
+# def transaction(category:Category,value):
+#     category.money = category.money + value
+#     category.jar.money = category.jar.money + value
+
+# anuong = Category("anuong",jar1)
+# dichuyen = Category("dichuyen",jar1)
+
+# transaction(anuong,100)
+# transaction(dichuyen,200)
+
+# print(anuong.money)
+# print(dichuyen.money)
+# print(jar1.money)
